9189ece26b085429dc682adf1b03e2b6f1e88424
Marc Kelechava <marc.kelechava@gmail.com>
2021-12-01 22:40:21 -0800

    day2 part2

--- solutions/day2.py
+++ solutions/day2.py
@@ -12,8 +12,24 @@ def part1(data):
     return counter["forward"] * (counter["down"] - counter["up"])
 
 
+def part2(data):
+    depth, position, aim = 0,0,0
+    for move in [line.split() for line in data]:
+        direction, amount = move
+        amount = int(amount)
+        if direction == "forward":
+            depth += (aim * amount)
+            position += amount
+        elif direction == "down":
+            aim += amount
+        else:
+            aim -= amount
+    return position * depth
+
 if __name__ == "__main__":
     data = read_input("input/test/day2.txt")
     assert part1(data) == 150
     full_data = read_input("input/day2.txt")
     print(f"Part 1: {part1(full_data)}")
+    assert part2(data) == 900
+    print(f"Part 2: {part2(full_data)}")
