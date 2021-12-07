fc7b58a6a61d593ef0c7ff5d07d486411dfd8fc3
Marc Kelechava <marc.kelechava@gmail.com>
2021-12-06 21:30:44 -0800

    day6 add fast approach pt1 pt2

--- solutions/day6.py
+++ solutions/day6.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from util import read_input
 from dataclasses import dataclass
 
@@ -8,15 +9,29 @@ class Lanternfish:
 def simulate_spawn(inital_fish_timer_list, days=18):
     fish_school = [Lanternfish(t) for t in inital_fish_timer_list]
     for _ in range(days):
-        for fish in list(fish_school):
+        for fish in fish_school.copy():
             fish.timer -=1
             if fish.timer == -1:
                 fish.timer = 6
                 fish_school.append(Lanternfish(8))
     return len(fish_school)
+
+def simulate_spawn_fast(initial_fish_timer_list, days=18):
+    fish_school = Counter()
+    fish_school.update(initial_fish_timer_list)
+    for _ in range(days):
+        for i in range(0, 9):
+            fish_school[i-1] = fish_school[i]
+        fish_school[8] = 0
+        fish_school[6] += fish_school[-1]
+        fish_school[8] += fish_school[-1]
+        fish_school[-1] = 0    
+    return sum(fish_school.values())
+
 if __name__ == "__main__":
     test_data = read_input("day6", single_line=True, test=True, as_int=True)
-    simulate_spawn(test_data, days=80) == 5934
+    assert simulate_spawn_fast(test_data, days=80) == 5934
     data = read_input("day6", single_line=True, as_int=True)
-    print(f"Part 1: {simulate_spawn(data, days=80)}")
-    # simulate_spawn(test_data, days=256) == 26984457539
+    print(f"Part 1: {simulate_spawn_fast(data, days=80)}")
+    assert simulate_spawn_fast(test_data, days=256) == 26984457539
+    print(f"Part 2: {simulate_spawn_fast(data, days=256)}")
