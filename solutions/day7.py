97c7dfdff2c8a590f068cd8309e9c90ee667f0e9
Marc Kelechava <marc.kelechava@gmail.com>
2021-12-07 21:37:36 -0800

    py day7 pt2

--- solutions/day7.py
+++ solutions/day7.py
@@ -1,5 +1,6 @@
 from os import read
 from util import read_input
+from collections import Counter
 
 def median(data):
     if len(data) % 2 == 1:
@@ -9,9 +10,28 @@ def median(data):
         midpoint = int(len(data) / 2)
         return int(sum(data[midpoint-1:midpoint+1])/2)
 
+def fuel_cost(data):
+    cost_counter = Counter()
+    cost = 0
+    for i in range(1, max(data)+1):
+        cost += i
+        cost_counter[i] = cost
+    return cost_counter
+
 def part1(data):
     median_point = median(data)
     return sum(abs(n - median_point) for n in data)
+
+def part2(data):
+    fuel_cost_map = fuel_cost(data)
+    total_cost = Counter()
+    for candidate in range(min(data), max(data)):
+        candidate_cost = 0
+        for n in data:
+            candidate_cost += fuel_cost_map[abs(n - candidate)]
+        total_cost[candidate] = candidate_cost
+    position = [candidate for candidate, cost in total_cost.items() if cost == min(total_cost.values())][0]
+    return total_cost[position]
 
 
 if __name__ == "__main__":
@@ -19,3 +39,5 @@ if __name__ == "__main__":
     assert part1(test_data) == 37
     data = read_input("day7", as_int=True, single_line=True)
     print(f"Part 1: {part1(data)}")
+    assert  part2(test_data) == 168
+    print(f"Part 2: {part2(data)}")
